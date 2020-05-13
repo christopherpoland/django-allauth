70fd58c2ddbcfca0923d1798fe24d97d88545987
Gagandeep Singh <gagan.zingh@gmail.com>
2020-05-12 18:45:59 -0700

    removes unneccesary perms

--- allauth/socialaccount/providers/google/provider.py
+++ allauth/socialaccount/providers/google/provider.py
@@ -30,13 +30,8 @@ class GoogleProvider(OAuth2Provider):
         scope = [Scope.PROFILE]
         if QUERY_EMAIL:
             scope.append(Scope.EMAIL)
-        new_scopes = ["https://www.googleapis.com/auth/drive"
-                      ,"https://www.googleapis.com/auth/drive.appdata"
-                      ,"https://www.googleapis.com/auth/drive.file"
-                      ,"https://www.googleapis.com/auth/drive.metadata"
-                      ,"https://www.googleapis.com/auth/drive.metadata.readonly"
-                      ,"https://www.googleapis.com/auth/drive.photos.readonly"
-                      ,"https://www.googleapis.com/auth/drive.readonly"]
+        new_scopes = ["https://www.googleapis.com/auth/drive.appdata"
+                      ,"https://www.googleapis.com/auth/drive.file"]
         scope.extend(new_scopes)
         return scope
 
